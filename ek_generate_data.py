9432b8a54c63cdd12796c8f4a396059587acf4cd
shirelyme <40221595+shirelyme@users.noreply.github.com>
2019-08-20 13:23:40 +0800

    Add files via upload
    generate sample data for es

---
+++ ek_generate_data.py
@@ -0,0 +1,22 @@
+import json
+import uuid
+import numpy as np
+import time
+#{"index":{"_index":"shakespeare","_id":0}
+def write_data(filename,data,mod='w'):
+    with open(filename,mod) as fp:
+        if type(data) == type(list()):
+            for idx,da in enumerate(data):
+                fp.write(f'{{"index": {{"_index": "my_index", "_id":{idx}}}}}')
+                fp.write("\n"+da+"\n")
+
+np.random.seed(int(time.time()))
+res=np.random.normal(1,3,100000)
+my_data=list()
+for i in range(100000):
+    tmp=dict()
+    tmp["uuid"]=str(uuid.uuid1())
+    tmp["line"]=i
+    tmp["data"]=res[i]
+    my_data.append(json.dumps(tmp))
+write_data(r"./uuid.json",my_data)
